2f74a2280c6bb385c235046970a140c1858c0848
carabasdaniel <daniel.carabas@hp.com>
2016-01-22 18:00:00 +0200

    Remove random string from service name and set default plan free

--- lib/mgmt/configure_usb_mgmt.go
+++ lib/mgmt/configure_usb_mgmt.go
@@ -499,6 +499,7 @@ func ConfigureAPI(api *UsbMgmtAPI, auth authentication.AuthenticationInterface, 
 		plan.ID = uuid.NewV4().String()
 		plan.Description = "default plan"
 		plan.Name = "default"
+		plan.Free = true
 
 		var meta brokerapi.ServicePlanMetadata
 
@@ -522,7 +523,7 @@ func ConfigureAPI(api *UsbMgmtAPI, auth authentication.AuthenticationInterface, 
 		var service brokerapi.Service
 
 		service.ID = uuid.NewV4().String()
-		service.Name = fmt.Sprintf("%s-%s", params.DriverInstance.Name, GetRandomString(5))
+		service.Name = params.DriverInstance.Name
 		service.Description = "Default service"
 		service.Tags = []string{params.DriverInstance.Name}
 		service.Bindable = true
