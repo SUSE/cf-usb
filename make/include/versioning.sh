de9b6026dbddd5ff3ae9033f73da59a15aef0074
Andreas Kupries <andreas.kupries@suse.com>
2017-12-01 11:16:09 -0800

    Fix hcf references.

--- make/include/versioning.sh
+++ make/include/versioning.sh
@@ -14,7 +14,7 @@ GIT_TAG=${GIT_TAG:-$(echo ${GIT_DESCRIBE} | awk -F - '{ print $1 }' )}
 GIT_COMMITS=${GIT_COMMITS:-$(echo ${GIT_DESCRIBE} | awk -F - '{ print $2 }' )}
 GIT_SHA=${GIT_SHA:-$(echo ${GIT_DESCRIBE} | awk -F - '{ print $3 }' )}
 
-ARTIFACT_NAME=${ARTIFACT_NAME:-$(basename $(git config --get remote.origin.url) .git | sed s/^hcf-//)}
+ARTIFACT_NAME=${ARTIFACT_NAME:-$(basename $(git config --get remote.origin.url) .git | sed s/^scf-//)}
 
 if [ -z "${GIT_COMMITS}" ] && [ -z "${GIT_SHA}" ] 
     then
